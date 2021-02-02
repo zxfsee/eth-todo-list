9e684a2bbea51c561e551f614c858f4971514e0d
sw1max <w1maxgg@gmail.com>
2021-02-02 17:56:25 +0300

    2. List tasks

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,15 @@
 module.exports = {
   networks: {
-    networks: {
-      development: {
-        host: "127.0.0.1",
-        port: 7545,
-        network_id: "*" // Match any network id
-      }
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
     },
-    solc: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  }
-}
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+};
